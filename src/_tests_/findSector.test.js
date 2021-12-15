bfb7dce5eba8b395a2032e05c326650b88c2abff
LorriAnn <LorriAnn7@att.net>
2021-12-14 21:17:33 -0500

    Determines probability level of designated coordinate'

--- src/_tests_/findSector.test.js
+++ src/_tests_/findSector.test.js
@@ -3,6 +3,7 @@ const {sector} = require('../components/findSector');
 test('sector of two coordinates', () => {
     expect(sector(100, 200)).toEqual(0);
     expect(sector(450, 220)).toEqual(2);
+    expect(sector(150, 550)).toEqual(1);
     expect(sector(500, 600)).toEqual(3);
     expect(sector(300, 500)).toEqual(0);
   });
--- src/components/findSector.js
+++ src/components/findSector.js
@@ -1,5 +1,42 @@
 
+  const defineSectors = {
+    sectors: [
+      {
+       upperLeft: [706, 276], 
+       upperRight: [436, 276],
+       lowerLeft: [706, 4],
+       lowerRight:[436, 4],
+       probability: 2,
+      },
+      {
+        upperLeft: [706, 706], 
+        upperRight: [436, 706],
+        lowerLeft: [706, 436],
+        lowerRight: [436, 436],
+        probability: 3,
+      },
+      {
+        upperLeft: [276, 706], 
+        upperRight: [4, 706],
+        lowerLeft: [276, 436],
+        lowerRight: [4, 436],
+        probability: 1,
+       },
+    ]
+  }
+
 const sector = (x, y) => {
+    for (i = 0; i < defineSectors.sectors.length; i++) {
+      if (
+          x >= defineSectors.sectors[i].upperRight[0] && 
+          x <= defineSectors.sectors[i].upperLeft[0] &&
+          y >= defineSectors.sectors[i].lowerLeft[1] &&
+          y <= defineSectors.sectors[i].upperLeft[1]
+          ) 
+      {
+          return defineSectors.sectors[i].probability;
+        } 
+    };
     return 0;
 };
 

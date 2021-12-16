d377363abeb4b3edde808ba6e99e0fbbfa8c389e
LorriAnn <LorriAnn7@att.net>
2021-12-15 20:37:53 -0500

    Places sector data in a json file

--- src/components/findSector.js
+++ src/components/findSector.js
@@ -1,40 +1,16 @@
 
-  const defineSectors = {
-    sectors: [
-      {
-       upperLeft: [706, 276], 
-       upperRight: [436, 276],
-       lowerLeft: [706, 4],
-       lowerRight:[436, 4],
-       probability: 2,
-      },
-      {
-        upperLeft: [706, 706], 
-        upperRight: [436, 706],
-        lowerLeft: [706, 436],
-        lowerRight: [436, 436],
-        probability: 3,
-      },
-      {
-        upperLeft: [276, 706], 
-        upperRight: [4, 706],
-        lowerLeft: [276, 436],
-        lowerRight: [4, 436],
-        probability: 1,
-       },
-    ]
-  }
+  const defineSectors = require('./sectors.json');
 
 const sector = (x, y) => {
-    for (i = 0; i < defineSectors.sectors.length; i++) {
+    for (i = 0; i < defineSectors.length; i++) {
       if (
-          x >= defineSectors.sectors[i].upperRight[0] && 
-          x <= defineSectors.sectors[i].upperLeft[0] &&
-          y >= defineSectors.sectors[i].lowerLeft[1] &&
-          y <= defineSectors.sectors[i].upperLeft[1]
+          x >= defineSectors[i].right && 
+          x <= defineSectors[i].left &&
+          y >= defineSectors[i].bottom &&
+          y <= defineSectors[i].top
           ) 
       {
-          return defineSectors.sectors[i].probability;
+          return defineSectors[i].probability;
         } 
     };
     return 0;
